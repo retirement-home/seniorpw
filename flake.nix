941b0ba0b5776cef03b7fcb78a4e3da98b379210
Clara Rostock <clara@complexvector.space>
2026-03-07 23:31:48 +0100

    restructure output and format

--- flake.nix
+++ flake.nix
@@ -4,43 +4,53 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = {self, nixpkgs, flake-utils, ...}: flake-utils.lib.eachDefaultSystem (
-      system: let pkgs = nixpkgs.legacyPackages.${system}; in {
-      packages = {
-      default = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
-          name = "seniorpw";
-          src = builtins.path {
-            path = ./.;
-            name = "source";
-          };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = rec {
+          seniorpw = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+            name = "seniorpw";
+            src = builtins.path {
+              path = ./.;
+              name = "source";
+            };
 
-          sourceRoot = "source/src/seniorpw";
-          cargoLock = { 
-            lockFile = ./src/seniorpw/Cargo.lock;
-          };
+            sourceRoot = "source/src/seniorpw";
+            cargoLock = {
+              lockFile = ./src/seniorpw/Cargo.lock;
+            };
 
-          nativeBuildInputs = [ pkgs.installShellFiles ];
-          postInstall = ''
-          mandir="$releaseDir/build/senior-*/out/man/*.1"
-          for manfile in $mandir; do
-            installManPage "$manfile"
-          done
+            nativeBuildInputs = [ pkgs.installShellFiles ];
+            postInstall = ''
+              mandir="$releaseDir/build/senior-*/out/man/*.1"
+              for manfile in $mandir; do
+                installManPage "$manfile"
+              done
 
-          installShellCompletion --bash ../completions/senior.bash
-          installShellCompletion --zsh ../completions/senior.zsh
-          '';
+              installShellCompletion --bash ../completions/senior.bash
+              installShellCompletion --zsh ../completions/senior.zsh
+            '';
 
-          meta = {
-          description = "Password manager using age as backend; inspired by pass";
-          homepage = "https://gitlab.com/retirement-home/seniorpw";
-          };
+            meta = {
+              description = "Password manager using age as backend; inspired by pass";
+              homepage = "https://gitlab.com/retirement-home/seniorpw";
+            };
 
-          SENIORPW_ALT_MANDIR = "1";
+            SENIORPW_ALT_MANDIR = "1";
+          });
+
+          default = seniorpw;
+        };
       }
-      );
-
-
-      };
-      }
-  );
+    );
 }
